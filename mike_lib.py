69ad2ef2b4428bee776408a47763d0cb882d106c
Michael May <mike@ull.at>
2018-11-07 17:14:43 +0100

    Support for multiple sensors

--- mike_lib.py
+++ mike_lib.py
@@ -35,13 +35,22 @@ def sleep_rtc(sleep_for_sec):
     machine.deepsleep()
 
 
-def query_sensor():
+def query_sensor(pinSda, pinScl):
     import bme280
 
-    i2c = machine.I2C(sda=machine.Pin(0), scl=machine.Pin(2))
+    i2c = machine.I2C(sda=machine.Pin(pinSda), scl=machine.Pin(pinScl))
     bme = bme280.BME280(i2c=i2c)
+    raw = bme.read_compensated_data()
     print("query sensors: {}".format(bme.values))
-    return bme.read_compensated_data()
+
+    # Temperature: 1/100 Degree Celsius
+    temp = raw[0] / 100
+    # Pressure: 1/256 Pascal -> 1/100 hPa
+    pres = raw[1] / 256 / 100
+    # Humidity: 1/1024 % relative humidity
+    humi = raw[2] / 1024
+
+    return (temp, pres, humi)
 
 
 def send_to_graphite(data):
@@ -73,30 +82,56 @@ def send_to_graphite(data):
     sock.close()
 
 
-def _run_test():
+def _run_indoor():
     led = machine.Pin(16, machine.Pin.OUT)
     led.off()
 
-    sensor = query_sensor()
-    # Temperature: 1/100 Degree Celsius
-    temp = sensor[0] / 100
-    # Pressure: 1/256 Pascal -> 1/100 hPa
-    pres = sensor[1] / 256 / 100
-    # Humidity: 1/1024 % relative humidity
-    humi = sensor[2] / 1024
+    sensor = query_sensor(0, 2)
+    temperature = sensor[0]  # Degree Celsius
+    pressure    = sensor[1]  # hPa
+    humidity    = sensor[2]  # relative humidity
 
     #room = "kids_room_"  # ws://192.168.66.54:8266/
     room = "bed_room_"  # b4:e6:2d:36:db:28 ws://192.168.66.45:8266/
     #room = "living_room_"  # b4:e6:2d:37:38:3e ws://192.168.66.34:8266/
     data = [
-        (room + "temperature", temp),
-        (room + "pressure", pres),
-        (room + "humidity", humi)
+        (room + "temperature", temperature),
+        (room + "pressure", pressure),
+        (room + "humidity", humidity)
     ]
     send_to_graphite(data)
 
     led.on()
 
+
+def run_outdoor():
+    led = machine.Pin(16, machine.Pin.OUT)
+    led.off()
+
+    sensor1 = query_sensor(0, 2)
+    temperature1 = sensor1[0]  # Degree Celsius
+    pressure1    = sensor1[1]  # hPa
+    humidity1    = sensor1[2]  # relative humidity
+
+    sensor2 = query_sensor(4, 5)
+    temperature2 = sensor2[0]  # Degree Celsius
+    pressure2 = sensor2[1]  # hPa
+    humidity2 = sensor2[2]  # relative humidity
+
+    print("sensor1: {}".format(sensor1))
+    print("sensor2: {}".format(sensor2))
+
+    #room = "kids_room_"  # ws://192.168.66.54:8266/
+    #room = "bed_room_"  # b4:e6:2d:36:db:28 ws://192.168.66.45:8266/
+    #room = "living_room_"  # b4:e6:2d:37:38:3e ws://192.168.66.34:8266/
+    #data = [
+    #    (room + "temperature", temperature),
+    #    (room + "pressure", pressure),
+    #    (room + "humidity", humidity)
+    #]
+    #send_to_graphite(data)
+
+    led.on()
 
 def run_test1():
     set_time_by_ntp()
@@ -104,7 +139,7 @@ def run_test1():
     # Virtual (RTOS-based) timers with callback
     tim = machine.Timer(-1)
     sleep_for_ms = 10000
-    tim.init(period=sleep_for_ms, mode=machine.Timer.PERIODIC, callback=lambda t: _run_test())
+    tim.init(period=sleep_for_ms, mode=machine.Timer.PERIODIC, callback=lambda t: _run_indoor())
 
 
 def run_test2():
@@ -113,5 +148,5 @@ def run_test2():
     wait_for = 60 * 30  # every half hour
 
     while True:
-        _run_test()
+        _run_indoor()
         sleep(wait_for)
