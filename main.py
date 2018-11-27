1ef0a791aed2a96708f5401ee0db3c27321cbe4b
Michael May <mike@ull.at>
2018-11-27 21:37:44 +0100

    use local ntp, exception handling,...

---
+++ main.py
@@ -0,0 +1,3 @@
+import mike_lib
+
+mike_lib.run_loop()
--- mike_lib.py
+++ mike_lib.py
@@ -10,11 +10,11 @@ def get_mac_address():
 
 
 def set_time_by_ntp():
-    import ntptime
+    import mike_ntptime
 
     # set RealTimeClock with Network Time Protocol
     print("set RC with ntp")
-    return ntptime.settime()
+    return mike_ntptime.settime("192.168.66.100")
 
 
 def sleep(sleep_for_sec):
@@ -82,7 +82,7 @@ def send_to_graphite(data):
     sock.close()
 
 
-def _run_indoor():
+def _run_indoor(room, send=True):
     led = machine.Pin(16, machine.Pin.OUT)
     led.off()
 
@@ -91,20 +91,18 @@ def _run_indoor():
     pressure    = sensor[1]  # hPa
     humidity    = sensor[2]  # relative humidity
 
-    #room = "kids_room_"  # ws://192.168.66.54:8266/
-    room = "bed_room_"  # b4:e6:2d:36:db:28 ws://192.168.66.45:8266/
-    #room = "living_room_"  # b4:e6:2d:37:38:3e ws://192.168.66.34:8266/
     data = [
         (room + "temperature", temperature),
         (room + "pressure", pressure),
         (room + "humidity", humidity)
     ]
-    send_to_graphite(data)
+    if send:
+        send_to_graphite(data)
 
     led.on()
 
 
-def run_outdoor():
+def _run_outdoor(room, send=True):
     led = machine.Pin(16, machine.Pin.OUT)
     led.off()
 
@@ -118,37 +116,73 @@ def run_outdoor():
     pressure2 = sensor2[1]  # hPa
     humidity2 = sensor2[2]  # relative humidity
 
-    #room = "kids_room_"  # ws://192.168.66.54:8266/
-    #room = "bed_room_"  # b4:e6:2d:36:db:28 ws://192.168.66.45:8266/
-    #room = "living_room_"  # b4:e6:2d:37:38:3e ws://192.168.66.34:8266/
-    #data = [
-    #    (room + "temperature", temperature),
-    #    (room + "pressure", pressure),
-    #    (room + "humidity", humidity)
-    #]
-    #send_to_graphite(data)
+    data1 = [
+        (room + "sensor1_temperature", temperature1),
+        (room + "sensor1_pressure", pressure1),
+        (room + "sensor1_humidity", humidity1)
+    ]
+    if send:
+        send_to_graphite(data1)
+
+    data2 = [
+        (room + "sensor2_temperature", temperature2),
+        (room + "sensor2_pressure", pressure2),
+        (room + "sensor2_humidity", humidity2)
+    ]
+    if send:
+        send_to_graphite(data2)
 
     led.on()
 
-def run_outdoor_loop():
-    #every = 60 * 30  # every half hour
-    every = 20
+def run_loop():
+    every = 60 * 30  # every half hour
+    #every = 20
+
+    # room = "kids_room_"  # ws://192.168.66.103:8266/
+    # room = "bed_room_"
+    room = "living_room_"  # b4:e6:2d:37:38:3e ws://192.168.66.101:8266/
+    # room = "outdoor_"  # b4:e6:2d:36:db:28 ws://192.168.66.102:8266/
+    _run = _run_indoor
+    #_run = _run_outdoor
+
+    print("Starting loop for {} every {}".format(room, every))
+
+    # repeat until we get a valid time
+    while True:
+        try:
+            set_time_by_ntp()
+        except OSError:
+            print("ERROR: couldn't set time by ntp")
+            sleep(5)
+        else:
+            break
+
+    _run(room, False)
 
     while True:
-        #set_time_by_ntp()
         now_ut = utime.time()
         sec_passed = now_ut % every
         sec_to_wait = every - sec_passed
 
         # RC is not very precise...
-        if sec_to_wait < (every/2):
-            sec_to_wait += every
+        #if sec_to_wait < (every/2):
+        #    sec_to_wait += every
 
         print("now: {}, wait for: {}".format(utime.localtime(now_ut), sec_to_wait))
 
         sleep(sec_to_wait)
 
-        run_outdoor()
+        try:
+            _run(room)
+        except OSError:
+            print("ERROR: couldn't query sensor")
+
+        # update time
+        try:
+            set_time_by_ntp()
+        except OSError:
+            print("ERROR: couldn't set time by ntp")
+
 
 def run_test1():
     set_time_by_ntp()
@@ -159,11 +193,4 @@ def run_test1():
     tim.init(period=sleep_for_ms, mode=machine.Timer.PERIODIC, callback=lambda t: _run_indoor())
 
 
-def run_test2():
-    set_time_by_ntp()
 
-    wait_for = 60 * 30  # every half hour
-
-    while True:
-        _run_indoor()
-        sleep(wait_for)
